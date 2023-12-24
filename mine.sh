009cbfff91cc7fb11352f0130de58a2e5d5b0d21
rizki0030 <122207241+rizki0030@users.noreply.github.com>
2023-12-24 07:36:41 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RGfQGtwFdNpcgWBxHWaeiGHf4knc8UE4sr.asus01 -p x --cpu "$(nproc)"
